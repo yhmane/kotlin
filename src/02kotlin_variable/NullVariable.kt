8e77e15d75e187af224a8ceb6b8eb5ae50e806bd
Hwang Yunho <hwang9yh@gmail.com>
2020-08-05 22:49:12 +0900

    2. kotlin 변수와 자료형, 연산자

---
+++ src/02kotlin_variable/NullVariable.kt
@@ -0,0 +1,10 @@
+package `02kotlin_variable`
+
+fun main() {
+    var str: String? = "Hello World"
+    str = null
+    println("str1: $str") // str: null
+
+    //var str2: String = "Hello World"
+    //str2 = null  오류 str2는 null을 허용하지 않음
+}
---
+++ src/02kotlin_variable/Variable.kt
@@ -0,0 +1,14 @@
+package `02kotlin_variable`
+
+import kotlin.math.PI
+import kotlin.math.abs
+
+fun main() {
+    val intro: String = "안녕하세요!"
+    val num: Int = 20
+
+    println(PI)
+    println(abs(-12.6))
+
+    println("intro: $intro, num: $num")
+}
